e6f4ab8977ff14ffe4a4f3074ff154daa2cfedd6
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:40:01 +0200

    newtempl

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/my_app')
-def hello_world:
-	return render_template("my_app.html")
+@app.route('/')
+def hello_world():
+	return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
